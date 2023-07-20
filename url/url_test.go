4cd4ee4448d592204472a71bcb1e67322652c2ce
FahrianAfdholi <fahrianafdholi077@gmail.com>
2023-07-21 04:46:08 +0700

    update url_test.go

--- url/url_test.go
+++ url/url_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 )
 
-func TestWebWithoutCookies(t *testing.T) {
+func TestWeb(t *testing.T) {
 	app := fiber.New()
 
 	app.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
@@ -33,4 +33,30 @@ func TestWebWithoutCookies(t *testing.T) {
 	resp, err = app.Test(req)
 	require.NoError(t, err)
 	assert.Equal(t, 426, resp.StatusCode)
+}
+
+func TestAuthSession(t *testing.T) {
+	app := fiber.New()
+
+	app.Post("/login", controller.LoginUser)
+	app.Post("/register", controller.RegisterUser)
+	app.Get("/logout", func(c *fiber.Ctx) error {
+		c.ClearCookie("token")
+		return nil
+	})
+
+	req := httptest.NewRequest("POST", "/login", nil)
+	resp, err := app.Test(req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+
+	req = httptest.NewRequest("POST", "/register", nil)
+	resp, err = app.Test(req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+
+	req = httptest.NewRequest("POST", "/logout", nil)
+	resp, err = app.Test(req)
+	require.NoError(t, err)
+	assert.Equal(t, 405, resp.StatusCode)
 }
